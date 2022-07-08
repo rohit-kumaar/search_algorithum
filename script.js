7367fd6e695fb77290d3effd7a862bd6f26c7ea7
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-08 16:04:03 +0530

    all element

--- script.js
+++ script.js
@@ -9,26 +9,47 @@ const mobilesData = async () => {
 const data = mobilesData();
 data
   .then((data) => {
-    let productNameList = "";
-    let productRamList = "";
+    let card = "";
     data.forEach((product) => {
-      productNameList += ` <div>
-                    <label><input type="checkbox" class="productName"/>${product.name}</label>
-              </div>`;
+      const productName = document.querySelector(".productName");
+      createElements(product.name, "name", productName);
+      const productRam = document.querySelector(".productRam");
+      createElements(product.ram, "ram", productRam);
 
-      productRamList += `<div>
-                            <label><input type="checkbox" />${product.ram}</label>
-                        </div>`;
+      card += ` <div class="card">
+                    <div>${product.brand}</div>
+                    <div>${product.name}</div>
+                    <div>${product.ram}</div>
+                </div>`;
     });
 
-    const productName = document.querySelector(".productName");
-    const productRam = document.querySelector(".productRam");
-    productName.innerHTML = productNameList;
-    productRam.innerHTML = productRamList;
+    const cards = document.querySelector(".cards");
+
+    cards.innerHTML = card;
   })
   .catch((err) => {
     console.log(err);
   });
 
+function createElements(product, className, appendClass) {
+  const div = document.createElement("div");
+  const label = document.createElement("label");
+  const input = document.createElement("input");
+  const span = document.createElement("span");
+  span.innerText = `${product}`;
+  input.type = "checkbox";
+  input.classList = className;
+  div.appendChild(label);
+  label.appendChild(input);
+  label.appendChild(span);
+  appendClass.appendChild(div);
+}
 
-  
+const checkbox = document.getElementsByClassName("name");
+console.log(checkbox);
+
+const ram = document.getElementsByClassName("ram");
+console.log(ram);
+
+const card = document.getElementsByClassName("card");
+console.log(card);
